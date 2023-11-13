80e13025ad5c8d7829fec3c7018a43d67aa80ffb
Israel Azoulay <israelazoulay@Liors-MacBook-Pro.local>
2023-11-13 20:20:44 +0200

    Updated the AWS CI pipeline to include the AWS region. Besides, updated the variable.tf file as well to include the AWS region

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -2,3 +2,8 @@ variable "key_name" {
   description = "The key name to use for the instance"
   default     = "eks-worker-nodes-keypair"
 }
+
+variable "region" {
+  description = "The AWS region to deploy resources into"
+  default     = "us-east-1"
+}
