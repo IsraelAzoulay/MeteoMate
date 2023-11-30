a51b3107b5aa16d0f06fd8ca50b7d95473c21866
Israel Azoulay <israelazoulay@Liors-MacBook-Pro.local>
2023-11-30 18:15:36 +0200

    Updated AWS ci/cd piplines to be triggered on push, and removed the Route53 record block from the acm.tf file

--- terraform/acm.tf
+++ terraform/acm.tf
@@ -12,8 +12,3 @@ resource "aws_acm_certificate" "cert" {
     create_before_destroy = true
   }
 }
-
-resource "aws_acm_certificate_validation" "cert" {
-  certificate_arn         = aws_acm_certificate.cert.arn
-  validation_record_fqdns = [aws_route53_record.cert_validation.fqdn]
-}
